2e903e932f6da91f441b78a12a11c84d5d9105f7
castleroad95 <68772019+castleroad95@users.noreply.github.com>
2021-03-04 17:55:04 +0900

    Delete lecture06.js

--- js/js/lecture06.js
+++
@@ -1,8 +0,0 @@
-var height=prompt('키를 입력해주세요.');
-
-var height_int=parseInt(height);
-console.log(height_int,typeof(height_int));
-
-
-var height_float=parseFloat(height);
-console.log(height_float,typeof(height_float));
