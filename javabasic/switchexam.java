c9d6ffe31aa3fae8bcbebb7ac008c06b8b2d3c47
castleroad95 <68772019+castleroad95@users.noreply.github.com>
2021-04-20 17:48:21 +0900

    Add files via upload

---
+++ javabasic/switchexam.java
@@ -0,0 +1,34 @@
+public class switchexam {
+    public static void main(String[] args){
+        /*
+        String str="A";
+
+        switch(str){
+            case "A":
+                System.out.println("A");
+            break;
+
+            case "B":
+                System.out.println("B");
+            break;
+
+            case "C":
+                System.out.println("C");
+            break;
+            default:
+                System.out.println("그외 다른 문자");
+        }
+    */
+
+    // 별만들기
+    
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+        System.out.println("");
+        }
+    }
+    
+    
+}
